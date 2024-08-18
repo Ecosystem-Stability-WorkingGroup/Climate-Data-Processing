649e3cb55ffbed4473c550ac074597b2d6c66e5e
Joan Dudney <jdudney@berkeley.edu>
2024-08-17 22:34:07 -0600

    pushing a script to connect with google drive

---
+++ Scripts/GoogleDriveLink.R
@@ -0,0 +1,32 @@
+## ---------------------------
+##
+## Script name: Connect to Google Drive
+##
+## Author: Dr. Joan Dudney
+##
+## Date Created: 2024-08-17
+##
+## Copyright (c) Joan Dudney, 2024
+## Email: dudney@ucsb.edu
+##
+## ---------------------------
+##
+## Notes: This code creates a connection to 
+##    Google Drive for desktop, allowing you to read in data stored on google drive without
+##    having to store it locally on you computer
+## ---------------------------
+
+
+librarian::shelf(R.utils, here, readr, progress)
+
+# Define the path to your local code directory
+code_dir <- here("Data")
+
+# Change this path so that it defines the path to your local google drive data folder
+data_dir <- "/Users/treelife/Library/CloudStorage/GoogleDrive-dudney@ucsb.edu/My Drive/Data"
+
+## create a folder called "Remote" that directly links to the shared google drive
+createLink(paste0(code_dir, '/Remote'), data_dir, overwrite = FALSE)
+
+
+test <- read_csv(here("Data", "Remote", "Climate Data", "spei.climate.dat.fin.csv"))
